aaaa64ca2d25fb45a6592d87a0a55844487f7c22
chrisu-1912 <121934971+santoschrisjay@users.noreply.github.com>
2023-12-03 13:41:33 +0800

    All good word to pdf
    fully functional

--- app.js
+++ app.js
@@ -347,6 +347,79 @@ app.get("/word-to-pdf", (req, res) =>
 	checkIfLogined(res, "document-converter/wordToPdf.ejs")
 );
 
+app.post("/convertWordToPdf", (req, res) => {
+	var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+
+	// Configure API key authorization: Apikey
+	var Apikey = defaultClient.authentications["Apikey"];
+	Apikey.apiKey = "3b6bfd19-9a46-44f6-91b6-a5a9b0e7b30c";
+
+	var apiInstance = new CloudmersiveConvertApiClient.ConvertDocumentApi();
+
+	const directoryPath = "";
+	const fileName = "";
+	const filePath = `${directoryPath}\\${fileName}`;
+
+	fs.readFile(filePath, (err, data) => {
+		if (err) {
+			console.error(`Error reading file ${filePath}: ${err}`);
+		} else {
+			const inputFile = Buffer.from(data.buffer);
+
+			var callback = function (error, responseData, response) {
+				if (error) {
+					console.error(error);
+				} else {
+					console.log(
+						"API called successfully. Returned data: " + responseData
+					);
+					// Handle the converted data as needed
+				}
+			};
+
+			apiInstance.convertDocumentDocxToPdf(inputFile, callback);
+		}
+	});
+	// Check if a file was uploaded
+	if (!req.files || !req.files.wordFile) {
+		return res.status(400).send("No Word file uploaded.");
+	}
+
+	// Get the uploaded PDF file from the request
+	const wordFile = req.files.wordFile;
+
+	// Read the contents of the PDF file
+	const inputFile = Buffer.from(wordFile.data.buffer);
+
+	// Call the Cloudmersive API to convert the PDF to Word
+	apiInstance.convertDocumentDocxToPdf(
+		inputFile,
+		(error, responseData, response) => {
+			if (error) {
+				console.error(error);
+				return res.status(500).send("Error converting Word to Pdf.");
+			}
+
+			// Handle the converted data as needed
+			const docxData = Buffer.from(responseData);
+
+			// Set the appropriate headers for download
+			res.setHeader(
+				"Content-Disposition",
+				"attachment; filename=converted-document.pdf"
+			);
+			res.setHeader(
+				"Content-Type",
+				"application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+			);
+
+			// Send the converted DOCX file as the response
+			res.status(200).send(docxData);
+		}
+	);
+});
+
+//**PDF TO WORD */
 app.get("/pdf-to-word", (req, res) =>
 	checkIfLogined(res, "document-converter/pdfToWord.ejs")
 );
