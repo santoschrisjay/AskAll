d2c63625a58ed16d3a3976276fbccd1d3b759886
JustinZap1 <151859724+JustinZap1@users.noreply.github.com>
2023-12-13 17:04:02 +0800

    All features

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.set("view engine", "ejs");
 const connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "hehez190",
+	password: "weakka12",
 	port: 3306,
 	database: "askalldb",
 });
@@ -914,75 +914,148 @@ app.get("/pdf-to-word", (req, res) =>
 );
 
 app.post("/convertPdfToWord", (req, res) => {
-	var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+	let convertTo = req.body.converTo;
 
-	// Configure API key authorization: Apikey
-	var Apikey = defaultClient.authentications["Apikey"];
-	Apikey.apiKey = "3b6bfd19-9a46-44f6-91b6-a5a9b0e7b30c";
+	if (convertTo == "Word to pdf"){
+		var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
 
-	var apiInstance = new CloudmersiveConvertApiClient.ConvertDocumentApi();
-
-	const directoryPath = "";
-	const fileName = "";
-	const filePath = `${directoryPath}\\${fileName}`;
-
-	fs.readFile(filePath, (err, data) => {
-		if (err) {
-			console.error(`Error reading file ${filePath}: ${err}`);
-		} else {
-			const inputFile = Buffer.from(data.buffer);
-
-			var callback = function (error, responseData, response) {
+		// Configure API key authorization: Apikey
+		var Apikey = defaultClient.authentications["Apikey"];
+		Apikey.apiKey = "3b6bfd19-9a46-44f6-91b6-a5a9b0e7b30c";
+	
+		var apiInstance = new CloudmersiveConvertApiClient.ConvertDocumentApi();
+	
+		const directoryPath = "";
+		const fileName = "";
+		const filePath = `${directoryPath}\\${fileName}`;
+	
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				console.error(`Error reading file ${filePath}: ${err}`);
+			} else {
+				const inputFile = Buffer.from(data.buffer);
+	
+				var callback = function (error, responseData, response) {
+					if (error) {
+						console.error(error);
+					} else {
+						console.log(
+							"API called successfully. Returned data: " + responseData
+						);
+						// Handle the converted data as needed
+					}
+				};
+	
+				apiInstance.convertDocumentDocxToPdf(inputFile, callback);
+			}
+		});
+		// Check if a file was uploaded
+		if (!req.files || !req.files.pdfFile) {
+			return res.status(400).send("No Word file uploaded.");
+		}
+	
+		// Get the uploaded PDF file from the request
+		const pdfFile = req.files.pdfFile;
+	
+		// Read the contents of the PDF file
+		const inputFile = Buffer.from(pdfFile.data.buffer);
+	
+		// Call the Cloudmersive API to convert the PDF to Word
+		apiInstance.convertDocumentDocxToPdf(
+			inputFile,
+			(error, responseData, response) => {
 				if (error) {
 					console.error(error);
-				} else {
-					console.log(
-						"API called successfully. Returned data: " + responseData
-					);
-					// Handle the converted data as needed
+					return res.status(500).send("Error converting Word to Pdf.");
 				}
-			};
-
-			apiInstance.convertDocumentPdfToDocx(inputFile, callback);
-		}
-	});
-	// Check if a file was uploaded
-	if (!req.files || !req.files.pdfFile) {
-		return res.status(400).send("No PDF file uploaded.");
-	}
-
-	// Get the uploaded PDF file from the request
-	const pdfFile = req.files.pdfFile;
-
-	// Read the contents of the PDF file
-	const inputFile = Buffer.from(pdfFile.data.buffer);
-
-	// Call the Cloudmersive API to convert the PDF to Word
-	apiInstance.convertDocumentPdfToDocx(
-		inputFile,
-		(error, responseData, response) => {
-			if (error) {
-				console.error(error);
-				return res.status(500).send("Error converting PDF to Word.");
+	
+				// Handle the converted data as needed
+				const docxData = Buffer.from(responseData);
+	
+				// Set the appropriate headers for download
+				res.setHeader(
+					"Content-Disposition",
+					"attachment; filename=converted-document.pdf"
+				);
+				res.setHeader(
+					"Content-Type",
+					"application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+				);
+	
+				// Send the converted DOCX file as the response
+				res.status(200).send(docxData);
 			}
+		);
+	}else{
+			var defaultClient = CloudmersiveConvertApiClient.ApiClient.instance;
+		// Configure API key authorization: Apikey
+		var Apikey = defaultClient.authentications["Apikey"];
+		Apikey.apiKey = "3b6bfd19-9a46-44f6-91b6-a5a9b0e7b30c";
 
-			// Handle the converted data as needed
-			const docxData = Buffer.from(responseData);
+		var apiInstance = new CloudmersiveConvertApiClient.ConvertDocumentApi();
 
-			// Set the appropriate headers for download
-			res.setHeader(
-				"Content-Disposition",
-				"attachment; filename=converted-document.docx"
-			);
-			res.setHeader(
-				"Content-Type",
-				"application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-			);
+		const directoryPath = "";
+		const fileName = "";
+		const filePath = `${directoryPath}\\${fileName}`;
 
-			// Send the converted DOCX file as the response
-			res.status(200).send(docxData);
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				console.error(`Error reading file ${filePath}: ${err}`);
+			} else {
+				const inputFile = Buffer.from(data.buffer);
+
+				var callback = function (error, responseData, response) {
+					if (error) {
+						console.error(error);
+					} else {
+						console.log(
+							"API called successfully. Returned data: " + responseData
+						);
+						// Handle the converted data as needed
+					}
+				};
+
+				apiInstance.convertDocumentPdfToDocx(inputFile, callback);
+			}
+		});
+		// Check if a file was uploaded
+		if (!req.files || !req.files.pdfFile) {
+			return res.status(400).send("No PDF file uploaded.");
 		}
-	);
+
+		// Get the uploaded PDF file from the request
+		const pdfFile = req.files.pdfFile;
+
+		// Read the contents of the PDF file
+		const inputFile = Buffer.from(pdfFile.data.buffer);
+
+		// Call the Cloudmersive API to convert the PDF to Word
+		apiInstance.convertDocumentPdfToDocx(
+			inputFile,
+			(error, responseData, response) => {
+				if (error) {
+					console.error(error);
+					return res.status(500).send("Error converting PDF to Word.");
+				}
+
+				// Handle the converted data as needed
+				const docxData = Buffer.from(responseData);
+
+				// Set the appropriate headers for download
+				res.setHeader(
+					"Content-Disposition",
+					"attachment; filename=converted-document.docx"
+				);
+				res.setHeader(
+					"Content-Type",
+					"application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+				);
+
+				// Send the converted DOCX file as the response
+				res.status(200).send(docxData);
+			}
+		);
+	}
 });
 
 //**unit-converter */
